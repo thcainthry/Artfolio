8f01c92d2e0ccb072ca2699d4a28f985e119b486
thcainthry <hasani.florinda@gmail.com>
2023-04-03 21:08:45 +0200

    server file created and listening on port 5000

---
+++ Server_Side/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Server is runing.')
+}
+)
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
