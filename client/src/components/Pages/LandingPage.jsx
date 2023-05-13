b215c68eea60e4b96b328964046210c88d2e7850
HMuhamed <50260119+HMuhamed@users.noreply.github.com>
2023-05-13 22:18:29 +0200

    Fixing code...

--- client/src/components/Pages/LandingPage.jsx
+++ client/src/components/Pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import LoginPage from '../pages/LoginPage.jsx';
-import SignUp from '../pages/SignUp.jsx';
+import LoginPage from '../Pages/LoginPage.jsx';
+import SignUp from '../Pages/SignUp.jsx';
 
 
 
