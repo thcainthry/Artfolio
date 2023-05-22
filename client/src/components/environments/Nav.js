e396283562f3a34fcbf131dded3759f78aa0e40b
MuratB17 <90219498+MuratB17@users.noreply.github.com>
2023-05-22 19:00:01 +0200

    fiiix

--- client/src/components/environments/Nav.js
+++ client/src/components/environments/Nav.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import "../style/Nav.modules.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+
 import 'boxicons/css/boxicons.min.css';
 import  '../../index1.css'
 
