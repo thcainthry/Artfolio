15faf0e2977d7b8b30047143ef82ecd6e34d2d90
MuratB17 <90219498+MuratB17@users.noreply.github.com>
2023-05-30 16:49:42 +0200

    fix image in home

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../style/Home.css';
 import Exhibition from './Exhibition.jsx';
+import Backgroundimage from '../assets/vllau.jpeg'
 
 const HomePage = () => {
   return (
+    <header style={ image}>
     <div className="container">
-      <div className="bg-image" />
+      
       <div className="content">
         <h1 className="title">Welcome to my Gallery</h1>
         <p className="subtitle">I am an artist and painter, creating beautiful pieces of art that reflect my passion and creativity. Browse my portfolio to discover a world of imagination and inspiration.</p>
@@ -14,7 +16,17 @@ const HomePage = () => {
         </a>
       </div>
     </div>
+    </header>
   );
 };
+const image = {
+
+  width: "100%",
+  height: "90vh",
+  background: `url(${Backgroundimage})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+}
 
 export default HomePage;
