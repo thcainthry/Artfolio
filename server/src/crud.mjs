003595c6021d8e28e25e67464230c510fdbd318c
DoresaO <doresa.osmanaj@student.uni-pr.edu>
2023-05-30 14:48:04 +0200

    changes made in main by doresa, forgot to switch branch/ fixed crud and createUserForm

--- server/src/crud.mjs
+++ server/src/crud.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
 import { config } from './db/credentials.mjs';
+import { data } from "jquery";
 
 const app = express();
 app.use(cors());
@@ -37,10 +38,35 @@ app.get("/ContactUsForm", (req, res) => {
   });
 });
 
+//is never used, and is called in the front but the front is never used
+app.get("/CreateUserForm",(req,res)=>{
+  const q="SELECT * FROM USERS;";
+  con.query(q,(err,data)=>{
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+     return res.json(data);
+  });
+});
+
 app.post("/ContactUsForm", (req, res) => {
   const { name, email, message } = req.body;
   const q = "INSERT INTO Na_kontaktos (name, email, message) VALUES (?, ?, ?)";
   con.query(q, [name, email, message], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    return res.json(result);
+  });
+});
+
+//is never used
+app.post("/CreateUserForm", (req, res) => {
+  const { emri, mbiemri, username, password, email, ditelindja } = req.body;
+  const q = "INSERT INTO Users (emri, mbiemri, username, password, email, ditelindja) VALUES (?, ?, ?, ?, ?, ?)";
+  con.query(q, [emri, mbiemri, username, password, email, ditelindja], (err, result) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: "Internal server error" });
