640e01c61c0a1f2dc48d5ce8db789c7c2cb486d8
MuratB17 <90219498+MuratB17@users.noreply.github.com>
2023-05-31 22:27:01 +0200

    update shoping cart at profile and change the proceed to checkout button

--- client/src/components/Pages/Profile.jsx
+++ client/src/components/Pages/Profile.jsx
@@ -1,9 +1,96 @@
-import React from 'react';
+
 import { AiOutlineUser, AiOutlineHome, AiOutlineGlobal, AiOutlineMail, AiOutlineLock } from 'react-icons/ai';
 import '../style/Profile.css';
+import React, {
+	useState,
+	useEffect,
+} from "react";
+import "../style/main.css";
+import {GiShoppingBag}  from "react-icons/gi";
+import ShoppingCart from "./ShoppingCart.jsx";
+function Profile() {
+    const [cartsVisibilty, setCartVisible] =
+    useState(false);
+const [productsInCart, setProducts] =
+    useState(
+        JSON.parse(
+            localStorage.getItem(
+                "shopping-cart"
+            )
+        ) || []
+    );
+useEffect(() => {
+    localStorage.setItem(
+        "shopping-cart",
+        JSON.stringify(productsInCart)
+    );
+}, [productsInCart]);
 
-const Profile = () => {
+
+const onQuantityChange = (
+    productId,
+    count
+) => {
+    setProducts((oldState) => {
+        const productsIndex =
+            oldState.findIndex(
+                (item) =>
+                    item.id === productId
+            );
+        if (productsIndex !== -1) {
+            oldState[productsIndex].count =
+                count;
+        }
+        return [...oldState];
+    });
+};
+
+const onProductRemove = (product) => {
+    setProducts((oldState) => {
+        const productsIndex =
+            oldState.findIndex(
+                (item) =>
+                    item.id === product.id
+            );
+        if (productsIndex !== -1) {
+            oldState.splice(productsIndex, 1);
+        }
+        return [...oldState];
+    });
+};
+
   return (
+    <div className="Buy">
+    <ShoppingCart
+        visibilty={cartsVisibilty}
+        products={productsInCart}
+        onClose={() =>
+            setCartVisible(false)
+        }
+        onQuantityChange={
+            onQuantityChange
+        }
+        onProductRemove={onProductRemove}
+    />
+    <div className="navbar">
+        <h3 className="logos">Profile</h3>
+        <button
+            className="btn shopping-cart-btn"
+            onClick={() =>
+                setCartVisible(true)
+            }>
+            <GiShoppingBag size={24} />
+            {productsInCart.length >
+                0 && (
+                <span className="product-count">
+                    {
+                        productsInCart.length
+                    }
+                </span>
+            )}
+        </button>
+    </div>
+    
     <div className="profile-container">
       <h2>Profile Form</h2>
       <form className="profile-form">
@@ -52,7 +139,9 @@ const Profile = () => {
         <button type="submit">Submit</button>
       </form>
     </div>
-  );
-};
+    </div>
+      );
+    }
+
 
 export default Profile;
